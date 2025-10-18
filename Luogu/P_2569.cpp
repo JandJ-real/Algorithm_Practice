e78e42fd171f408cecfa53e068d731024233ac70
JandJ-real <chenghuajie2006@outlook.com>
2025-10-18 10:30:01 +0800

    自动提交: 2025/10/18 周六 10:30:01.71

--- Luogu/P_2569.cpp
+++ Luogu/P_2569.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 #define int long long
-inline int read()
-{
+inline int read() {
     int x = 0, f = 1;     // x: 存储结果, f: 符号标志(1正数, -1负数)
     char ch = getchar();  // 读取第一个字符
 
@@ -37,19 +36,18 @@ vector<vector<int>> dp;
 
 */
 int ans;
-signed main()
-{
+signed main() {
     cin >> t >> maxp >> w;
     dp.resize(t + 1);
     for (int i = 1; i <= t; i++) {
-        dp[i].resize(maxp + 1, LONG_LONG_MIN);
+        dp[i].resize(maxp + 1, LLONG_MIN/4);
         buy[i] = read();
         sell[i] = read();
         buynum[i] = read();
         sellnum[i] = read();
     }
-    dp[0].resize(maxp + 1, LONG_LONG_MIN);
-    // dp[0][0] = 0;
+    dp[0].resize(maxp + 1, LLONG_MIN/4);
+    dp[0][0]=0;
     // dp[1][0] = 0;
     deque<int> de1;  //卖的，递减
     deque<int> de2;  //买的，递减
@@ -66,8 +64,9 @@ signed main()
         //----------------------
 
         for (int j = 0; j <= maxp; j++) {
-            if (i - 1 >= 0)
+            
                 dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+            
         }
         if (i <= w + 1)
             continue;
@@ -80,39 +79,42 @@ signed main()
         // Buynum = buynum[last];
         //卖的：
         for (int j = maxp; j >= 0; j--) {
-            //维护区间长度
-            while (!de1.empty() && de1.front() - Sellnum > j) {
+            while (!de1.empty() && de1.front() - j > Sellnum) {
                 de1.pop_front();
             }
+            if (!de1.empty())
+                dp[i][j] = max(dp[i][j], dp[last][de1.front()] + Sell * (de1.front() - j));
 
-            //维护单调性
             while (!de1.empty() && dp[last][de1.back()] + Sell * (de1.back()) < dp[last][j] + Sell * j) {
                 de1.pop_back();
             }
             de1.push_back(j);
-            //状态转移
-            if (!de1.empty())
-                dp[i][j] = max(dp[i][j], dp[last][de1.front()] + Sell * (de1.front() - j));
         }
         //买的
         for (int j = 0; j <= maxp; j++) {
-            while (!de2.empty() && Buynum + de2.front() < j) {
+            while (!de2.empty() && de2.front()+Buynum<j) {
                 de2.pop_front();
             }
+            if (!de2.empty())
+                dp[i][j] = max(dp[i][j], dp[last][de2.front()] + Buy * (de2.front() - j));
 
-            while (!de2.empty() && dp[last][de2.back()] - Buy * (de2.back()) < dp[last][j] - Buy * j) {
+
+                //惊天大bug：
+            /*
+            原来是： while (!de2.empty() && dp[last][de2.back()] - Buy * (de2.back()) < dp[last][j] - Buy * j)
+            这是根据常识想当然的转移，但是根据
+            dp[i][j] = max(dp[i][j], dp[last][de2.front()] + Buy * (de2.front() - j));
+要维护的最值显然是dp[last][de2.front()] + Buy * (de2.front()这一部分，是加号
+            */
+            while (!de2.empty() && dp[last][de2.back()] + Buy * (de2.back()) < dp[last][j] + Buy * j) {
                 de2.pop_back();
             }
             de2.push_back(j);
-            if (!de2.empty())
-                dp[i][j] = max(dp[i][j], dp[last][de2.front()] + Buy * (de2.front() - j));
-            cout << "i:" << i << ' ' << "j:" << j << ' ' << dp[i][j] << ' ';
         }
-        cout << endl;
     }
-
-    for (int i = 0; i <= maxp; i++) {
-        ans = max(ans, dp[t][i]);
-    }
+//问题：为什么最值不会是手中还持有股票的时候
+   for(int i=0;i<=maxp;i++){
+    ans=max(ans,dp[t][i]);
+   }
     cout << ans;
 }
